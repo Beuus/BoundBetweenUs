efb9b256b4557933c277a7d43d862a98e013af85
Beuus <56739308+Beuus@users.noreply.github.com>
2020-01-31 23:00:05 +0100

    Jumping Ok

--- Assets/Player/Jump.cs
+++ Assets/Player/Jump.cs
@@ -11,23 +11,41 @@ public class Jump : MonoBehaviour
     //[Range(0, 10)]
     //public float jumpingup = 2f;
 
-    [Range(0, 10)]
+    [Range(0, 5)]
     public float  jumpVelocity;
 
+    bool jumping;
+    public string jumpKey;
+
     private Rigidbody2D rb;
-    
+
+    void Start()
+    {
+        jumping = false;
+    }
+
     // Start is called before the first frame update
     void Awake()
     {
-        //rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+
+        if (Input.GetKeyDown(jumpKey) && !jumping)
         {
+            jumping = true;
             GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Floor"))
+        {
+            jumping = false;
         }
     }
 }
