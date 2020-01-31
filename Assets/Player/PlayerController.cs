08ae152a660af36e233800b62aed430a49d58b20
Beuus <56739308+Beuus@users.noreply.github.com>
2020-01-31 21:49:22 +0100

    Platform 2D first
    Create Proyect

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        //float verticalAxis = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalAxis, 0, 0) * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + movement);
+    }
+}
