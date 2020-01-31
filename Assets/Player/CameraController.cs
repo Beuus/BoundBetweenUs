cab5012e33964031ed3c102cf5aa2f2b5e938ca0
Mirza <mirzaojeda@gmail.com>
2020-01-31 23:19:37 +0100

    Añadida cámara que sigue al punto medio entre jugadores

--- Assets/Player/CameraController.cs
+++ Assets/Player/CameraController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject player1;
+    public GameObject player2;
+
     private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = ((transform.position - player1.transform.position) + (transform.position - player2.transform.position))/2;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player1.transform.position + (player2.transform.position - player1.transform.position)/2 + offset;
     }
 }
