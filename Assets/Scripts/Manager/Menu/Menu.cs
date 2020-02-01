ce49a714cc6f506b458b2a81fc1da0dfc07d2831
Beuus <56739308+Beuus@users.noreply.github.com>
2020-02-01 19:33:51 +0100

    Actualizacion y parte del manager

---
+++ Assets/Scripts/Manager/Menu/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _gameManager = GameManager.Instance;
+        _gameManager.OnStateChange += HandleOnStateChange;
+    }
+    public void HandleOnStateChange()
+    {
+        Debug.Log("Change!!");
+    }
+
+    public void StartGame()
+    {
+        //start game scene
+        _gameManager.SetGameState(GameState.GAME);
+        Debug.Log(_gameManager.gameState);
+        SceneManager.LoadScene("DesignScene");
+    }
+
+
+    public void ControlsGame()
+    {
+        //start game scene
+        _gameManager.SetGameState(GameState.CONTROLS);
+        Debug.Log(_gameManager.gameState);
+        SceneManager.LoadScene("Controls");
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
