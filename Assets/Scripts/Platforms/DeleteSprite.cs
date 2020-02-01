fb3d9b068360f1f68c1d6959d9ec34e665119688
Beuus <56739308+Beuus@users.noreply.github.com>
2020-02-01 17:18:40 +0100

    platforms

---
+++ Assets/Scripts/Platforms/DeleteSprite.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+    }
+}
