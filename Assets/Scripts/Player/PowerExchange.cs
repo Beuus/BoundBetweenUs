a00bbdd0fd8e836fd227cffd805f861e304f151d
Mirza <mirzaojeda@gmail.com>
2020-02-01 21:03:57 +0100

    Añadido control de trigger

--- Assets/Scripts/Player/PowerExchange.cs
+++ Assets/Scripts/Player/PowerExchange.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PowerExchange : MonoBehaviour
 {
+
+    private bool player1collision = false;
+    private bool player2collision = false;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Colliding with " + collision.gameObject.name);
-        if (collision.gameObject.tag.Equals("Player"))
+        if (collision.gameObject.name.Equals("Player1") && !player1collision || collision.gameObject.name.Equals("Player2") && !player2collision)
         {
             var jump = collision.gameObject.GetComponent<Jump>();
             var unbreakableRope = collision.gameObject.GetComponent<UnbreakableRope>();
@@ -16,18 +19,30 @@ public class PowerExchange : MonoBehaviour
                 Destroy(jump);
                 collision.gameObject.AddComponent<UnbreakableRope>();
                 if (collision.gameObject.name.Equals("Player1"))
+                {
                     collision.gameObject.GetComponent<UnbreakableRope>().input = "RopeUnbreakable1";
+                    player1collision = true;
+                }
                 else
+                {
                     collision.gameObject.GetComponent<UnbreakableRope>().input = "RopeUnbreakable2";
+                    player2collision = true;
+                }
             }
             else if (unbreakableRope != null)
             {
                 Destroy(unbreakableRope);
                 collision.gameObject.AddComponent<Jump>();
                 if (collision.gameObject.name.Equals("Player1"))
+                {
                     collision.gameObject.GetComponent<Jump>().input = "Jump1";
+                    player1collision = true;
+                }
                 else
+                {
                     collision.gameObject.GetComponent<Jump>().input = "Jump2";
+                    player2collision = true;
+                }
             }
         }
     }
