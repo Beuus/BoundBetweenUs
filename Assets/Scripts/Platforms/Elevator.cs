c311331694c832602ef4f10101ce38063d8bb2da
Mirza <mirzaojeda@gmail.com>
2020-02-01 15:39:42 +0100

    Añadido prototipo de plataforma con interruptor

---
+++ Assets/Scripts/Platforms/Elevator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject trigger;
+    public Transform target;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target.position && trigger.GetComponent<Switch>().activated)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
