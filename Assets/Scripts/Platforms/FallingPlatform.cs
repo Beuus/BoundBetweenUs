2f40ed1582225434f4e903840a4cd9ee0cd77d5d
Mirza <mirzaojeda@gmail.com>
2020-02-01 11:28:12 +0100

    Añadido prototipo de plataformas que se caen

---
+++ Assets/Scripts/Platforms/FallingPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private Rigidbody2D platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform = GetComponent<Rigidbody2D>();
+        platform.gravityScale = 0.0f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            platform.gravityScale = 1.0f;
+        }
+    }
+}
