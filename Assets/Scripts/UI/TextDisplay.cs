24018cc9b87a5500a7e22da7379c7685a4cfc001
Mirza <mirzaojeda@gmail.com>
2020-02-02 11:54:02 +0100

    Añadido prototipo de mensajes en pantalla

---
+++ Assets/Scripts/UI/TextDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplay : MonoBehaviour
+{
+    public string textToDisplay;
+    public GameObject trigger; 
+
+    private Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+        textComponent.text = textToDisplay;
+        textComponent.enabled = false;
+    }
+
+    void Update()
+    {
+        if (trigger.GetComponent<TextTrigger>().activated)
+        {
+            Debug.Log("Displaying text: " + textToDisplay);
+            textComponent.enabled = true;
+        }
+    }
+}
