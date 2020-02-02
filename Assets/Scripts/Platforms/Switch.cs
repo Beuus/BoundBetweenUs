9152a2d08b2e5c2329bf9b7a3ec83b91ff39031e
jetese <jetese2@gmail.com>
2020-02-02 14:21:36 +0100

    Arreglado Interruptor

--- Assets/Scripts/Platforms/Switch.cs
+++ Assets/Scripts/Platforms/Switch.cs
@@ -16,7 +16,7 @@ public class Switch : MonoBehaviour
         activated = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals(playerTag1) || collision.gameObject.tag.Equals(playerTag2))
         {
